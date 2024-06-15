f21ddc2e6f988785d199c399e345c1c7a19a4ef4
benben <hzcly98@gmail.com>
2024-06-15 20:52:43 +0800

    第八周作业

---
+++ 241-黄章成-深圳/第八周作业/hash.py
@@ -0,0 +1,96 @@
+import cv2
+import numpy as np
+import random
+
+
+# 均值哈希算法
+def aHash(img):
+    # 缩放为8*8
+    img = cv2.resize(img, (8, 8), interpolation=cv2.INTER_CUBIC)
+    # 转换为灰度图
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # s为像素和初值为0，hash_str为hash值初值为''
+    s = 0
+    hash_str = ''
+    # 遍历累加求像素和
+    for i in range(8):
+        for j in range(8):
+            s = s + gray[i, j]
+    # 求平均灰度
+    avg = s / 64
+    # 灰度大于平均值为1相反为0生成图片的hash值
+    for i in range(8):
+        for j in range(8):
+            if gray[i, j] > avg:
+                hash_str = hash_str + '1'
+            else:
+                hash_str = hash_str + '0'
+    return hash_str
+
+
+# 差值算法
+def dHash(img):
+    # 缩放8*9
+    img = cv2.resize(img, (9, 8), interpolation=cv2.INTER_CUBIC)
+    # 转换灰度图
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hash_str = ''
+    # 每行前一个像素大于后一个像素为1，相反为0，生成哈希
+    for i in range(8):
+        for j in range(8):
+            if gray[i, j] > gray[i, j + 1]:
+                hash_str = hash_str + '1'
+            else:
+                hash_str = hash_str + '0'
+    return hash_str
+
+
+# Hash值对比
+def cmpHash(hash1, hash2):
+    n = 0
+    # hash长度不同则返回-1代表传参出错
+    if len(hash1) != len(hash2):
+        return -1
+    # 遍历判断
+    for i in range(len(hash1)):
+        # 不相等则n计数+1，n最终为相似度
+        if hash1[i] != hash2[i]:
+            n = n + 1
+    return n
+
+
+# 图像加噪
+def addNoise(img):
+	rows, cols, channels = img.shape
+	noiseNum = random.randint(1, rows * cols)  # 随机噪声数
+	for i in range(noiseNum):
+		# 随机位置
+		row = random.randint(1, rows - 1)
+		col = random.randint(1, cols - 1)
+
+		for j in range(channels):
+			colorValue = random.randint(0, 255)
+			img[row, col, j] = colorValue
+	return img
+
+img1 = cv2.imread('lenna.png')
+# img2 = cv2.imread('lenna_noise.png')
+img2 = addNoise(img1.copy())
+hash1 = aHash(img1)
+hash2 = aHash(img2)
+print(hash1)
+print(hash2)
+n = cmpHash(hash1, hash2)
+print('均值哈希算法相似度：', n)
+
+hash1 = dHash(img1)
+hash2 = dHash(img2)
+print(hash1)
+print(hash2)
+n = cmpHash(hash1, hash2)
+print('差值哈希算法相似度：', n)
+
+cv2.imshow('src img', img1)
+cv2.imshow('noise img', img2)
+cv2.waitKey(0)
+cv2.destroyWindow()
