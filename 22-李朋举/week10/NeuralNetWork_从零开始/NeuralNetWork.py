5904145026a39a8846b262f3fbd77bfaa7c74550
lpj <17316177703@163.com>
2024-07-01 22:17:58 +0800

    2024-6-29 NeuralNetWork

--- 22-李朋举/week10/NeuralNetWork_从零开始/NeuralNetWork.py
+++ 22-李朋举/week10/NeuralNetWork_从零开始/NeuralNetWork.py
@@ -23,13 +23,20 @@ class NeuralNetWork:
         '''
         初始化权重矩阵: 由于权重不一定都是正的，它完全可以是负数，因此我们在初始化时，把所有权重初始化为-0.5到0.5之间
              这里两个权重矩阵，一个是wih(weight-input-hidden)表示输入层和中间层节点间链路权重形成的矩阵，
-                            一个是who(weight-hidden-output)表示中间层和输出层间链路权重形成的矩阵
-                            
-             numpy.random.rand(self.hnodes, self.inodes) =》0-1   -0.5 =》 -0.5-0.5
+                            一个是who(weight-hidden-output)表示中间层和输出层间链路权重形成的矩阵 
+                                   
+        self.wih = (numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes)))
+        作用是生成一个正态分布的随机数矩阵:
+            `numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))`：
+                    `numpy.random.normal()` 函数来生成一个正态分布的随机数矩阵。正态分布是一种常见的概率分布，其平均值为 `0.0`，
+                     标准差为 `pow(self.hnodes, -0.5)`。 (`self.hnodes` 的值取负的平方根(到数的平方根)，作为正态分布的标准差)
+             最后，`(self.hnodes, self.inodes)` 表示生成的随机数矩阵的大小，即行数为 `self.hnodes`，列数为 `self.inodes`。  
+             self.wih => ndarry(200，784) ndtype-float64
+             self.who => ndarry(10,200) ndtype-float64
         '''
+        # numpy.random.rand(self.hnodes, self.inodes) =》0-1   -0.5 =》 -0.5-0.5
         # self.wih = numpy.random.rand(self.hnodes, self.inodes) - 0.5
         # self.who = numpy.random.rand(self.onodes, self.hnodes) - 0.5
-
         self.wih = (numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes)))
         self.who = (numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes)))
 
@@ -52,11 +59,32 @@ class NeuralNetWork:
         第一步: 计算输入训练数据，给出网络的计算结果，这点跟我们前面实现的query()功能很像。"正向"
            把inputs_list(输入的训练数据), targets_list(训练数据对应的正确结果)转换成numpy支持的二维矩阵, T表示做矩阵的转置
         """
+        '''
+        将输入列表 `inputs_list` 转换为一个二维数组，并对其进行转置操作：
+            1. `numpy.array(inputs_list, ndmin=2)`：
+                使用 `numpy.array()` 函数将输入列表 `inputs_list` 转换为一个 `numpy` 数组。`ndmin=2` 参数表示数组的最小维度为 2，
+                即输入列表必须至少包含两个元素。如果输入列表的长度小于 2，则会引发 ValueError 异常。
+            2. `.T`：这部分代码对上一步生成的数组进行转置操作。转置操作会交换数组的行和列，使得原来的行变为列，原来的列变为行。
+            综上所述，这段代码的作用是将输入列表 `inputs_list` 转换为一个二维数组，并对其进行转置操作，以满足后续处理的要求。
+        inputs_list-> ndarray(784,)  [0.01  0.01 ...... 0.538  0.99223529, 0.99223529 0.99223529 0.83305882  0.01 ]
+        inputs -> ndarray:(784,1)    [[0.01      ], [0.01      ], [0.01      ], .......[0.01      ], [0.86023529], [0.99223529], [0.99223529], [0.16141176], [0.01      ] ]
+        '''
         inputs = numpy.array(inputs_list, ndmin=2).T
+        '''
+        targets_list-> ndarray(10,)  [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01]
+        targets-> ndarray:(10,1)     [[0.01], [0.01], [0.01], [0.01], [0.01], [0.99], [0.01], [0.01], [0.01], [0.01]]
+        '''
         targets = numpy.array(targets_list, ndmin=2).T
         # 计算信号经过输入层后产生的信号量  矩阵乘(AB的行列数必须满足要求)
+        """
+        self.wih => ndarry(200，784) dtype-float64     inputs => ndarray:(784,1) dtype-float64   
+        hidden_inputs => ndarry(200，1)  [[ 1.33109893e+00], [ 2.90960728e-01], [-8.80236886e-01],......, [-8.08880699e-01], [ 9.41510082e-01]]
+        """
         hidden_inputs = numpy.dot(self.wih, inputs)
         # 中间层神经元对输入的信号做激活函数后得到输出信号
+        """
+        hidden_outputs =>ndarry(200，1)  [[0.78637499], [0.3250882 ], [0.62135534], ......, [0.78473486], [0.51411813], [0.36048346]]
+        """
         hidden_outputs = self.activation_function(hidden_inputs)
         # 输出层接收来自中间层的信号量
         final_inputs = numpy.dot(self.who, hidden_outputs)
@@ -146,17 +174,9 @@ n = NeuralNetWork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 # 读入训练数据
 # open函数里的路径根据数据存储的路径来设定
 training_data_file = open("dataset/mnist_train.csv", 'r')
-training_data_list = training_data_file.readlines()
+training_data_list = training_data_file.readlines()  # training_data_list {list: 100} 每个元素都是一个str
 training_data_file.close()
-"""
-数据读取完毕后，再对数据格式做些调整，以便输入到神经网络中进行分析，需要做的是将数据“归一化”，也就是把所有数值全部转换到0.01到1.0之间。
-   由于表示图片的二维数组中，每个数大小不超过255，由此我们只要把所有数组除以255，就能让数据全部落入到0和1之间。 
-   有些数值很小，除以255后会变为0(信息丢失比较严重)，这样会导致链路权重更新出问题。
-   所以我们需要把除以255后的结果先乘以0.99，然后再加上0.01，这样所有数据就处于0.01到1之间。
-        image_array / 255.0  =》 0 - 1
-        image_array / 255.0 * 0.99 => 0 - 0.99
-        image_array / 255.0 * 0.99 + 0.01 =》 0 - 1
-"""
+
 
 '''
 加入epocs,设定网络的训练循环次数
@@ -167,19 +187,38 @@ epochs = 5
 for e in range(epochs):
     # 把数据依靠','区分，并分别读入
     for record in training_data_list:
+        """
+        record(str类型)  用逗号分隔  
+        all_values:['5', '0', '0', ......, '253', '253',  '0\n']
+        """
         all_values = record.split(',')
         # 第一个值对应的是图片的表示的数字，所以我们读取图片数据时要去掉第一个数值 （第一列为标签）
+        """
+        1.`numpy.asfarray()`主要作用是将输入转换为浮点数类型的`NumPy`数组, 不会改变输入数据的结构或形状，只是改变了数据的类型。
+               all_values[1:]  :['0', '0', ......, '253', '253',  '0\n']
+               numpy.asfarray(all_values[1:] :['0', '0', ......, '253.0', '253.0',  '0\n']
+        2.数据读取完毕后，再对数据格式做些调整，以便输入到神经网络中进行分析，需要做的是将数据“归一化”，也就是把所有数值全部转换到0.01到1.0之间。
+               由于表示图片的二维数组中，每个数大小不超过255，通过除以 255.0，可以将整数转换为浮点数，并将其值限制在 [0, 1] 之间。 
+               有些数值很小，除以255后会变为0(信息丢失比较严重)，这样会导致链路权重更新出问题。
+               所以我们需要把除以255后的结果先乘以0.99，然后再加上0.01，这样所有数据就处于[0.01, 1]之间,
+         以避免数据过于接近 0 或 1，从而可能导致数值计算的不稳定性。
+
+                image_array / 255.0  =》 0 - 1     
+                image_array / 255.0 * 0.99 => 0 - 0.99
+                image_array / 255.0 * 0.99 + 0.01 =》 0 - 1
+         inputs-> ndarray(784,)  [0.01  0.01 ...... 0.538  0.99223529, 0.99223529 0.99223529 0.83305882  0.01 ]
+        """
         inputs = (numpy.asfarray(all_values[1:])) / 255.0 * 0.99 + 0.01
         '''
         设置图片与数值的对应关系, 最外层有10个输出节点
         创建一个全0数组 +0.01 最小值为0.01 , 浮点数更准确(自定义)
-              [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01]
+              ndarray=(10,)   [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01]
         '''
         targets = numpy.zeros(output_nodes) + 0.01
         """
         all_values[0] 标签列, 将数字变成one-hot
-            如：   all_values[0] 是标签 7 
-               =》 [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.99, 0.01, 0.01]
+            如：   all_values[0] 是标签 5 
+               =》 [0.01 0.01 0.01 0.01 0.01 0.99 0.01 0.01 0.01 0.01]
         """
         targets[int(all_values[0])] = 0.99
         """
