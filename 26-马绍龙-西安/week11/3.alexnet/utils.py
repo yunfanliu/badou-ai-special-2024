285299784e43814636e90c184f87d86f4fdb1e6a
mashaolong <340541293@qq.com>
2024-07-04 23:16:47 +0800

    update  alexnet

--- 26-马绍龙-西安/week11/3.alexnet/utils.py
+++ 26-马绍龙-西安/week11/3.alexnet/utils.py
@@ -6,14 +6,24 @@ from tensorflow.python.ops import array_ops
 
 
 def load_image(path):
+
+    # 从路径读取图像
     # 读取图片，rgb
     img = mpimg.imread(path)
+
+    # 计算图像短边的长度
     # 将图片修剪成中心的正方形
     short_edge = min(img.shape[:2])
+
+    # 计算裁剪区域的起始坐标
     yy = int((img.shape[0] - short_edge) / 2)
     xx = int((img.shape[1] - short_edge) / 2)
+
+    # 裁剪图像中心的正方形区域
     crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
+
     return crop_img
+
 
 
 def resize_image(image, size):
@@ -28,7 +38,7 @@ def resize_image(image, size):
 
 def print_answer(argmax):
     with open("./index_word.txt", "r", encoding='utf-8') as f:
-        synset = [l.split(";")[1] for l in f.readlines()]
+        synset = [l.split(";")[1][:-1] for l in f.readlines()]
 
     # print(synset[argmax])
     return synset[argmax]
